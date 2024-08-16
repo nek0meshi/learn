f2ce3aeef138a643b3d4a825f01f0f4e7f429d46
nek0meshi <nekmeshi@gmail.com>
2024-08-16 11:06:45 +0900

    [lighthouse] show tasks

--- lighthouse/frontend/src/App.tsx
+++ lighthouse/frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { useQuery, gql } from '@apollo/client'
 import './App.css'
 
 function App() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      tasks {
+        id,
+        title,
+        status
+      }
+    }
+  `)
+
   return (
     <div>
       Hello World
+      {error && <div>error</div>}
+      {loading && <div>is loading...</div>}
+      {data && data.tasks.map((task: any) => (
+        <div key={task.id}>{task.title}</div>
+      ))}
     </div>
   )
 }
--- lighthouse/frontend/src/main.tsx
+++ lighthouse/frontend/src/main.tsx
@@ -1,10 +1,18 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
