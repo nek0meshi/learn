37d20c5b8e6625fe83338dae01d8672313e29af6
nek0meshi <nekmeshi@gmail.com>
2024-08-20 14:33:55 +0900

    [lighthouse] wip: LIFF

---
+++ lighthouse/README.md
@@ -0,0 +1,7 @@
+# lighthouse
+
+## 参考資料
+
+- [LINE LIFF](https://developers.line.biz/ja/docs/liff/overview/)
+- [lighthouse](https://lighthouse-php.com/)
+- [Material UI](https://mui.com/)
--- lighthouse/frontend/src/App.tsx
+++ lighthouse/frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import { useQuery, gql } from '@apollo/client'
+import { useEffect, useState } from 'react'
+import { liff } from '@line/liff'
 import { List, ListItem, Container } from '@mui/material'
 import './App.css'
+
+// TODO: @line/liffから直接取得したい
+type Profile = Awaited<ReturnType<typeof liff.getProfile>>
 
 interface Task {
   id: string
   title: string
   status: string
+}
+
+function login() {
+  liff.login()
 }
 
 function App() {
@@ -19,16 +28,54 @@ function App() {
     }
   `)
 
+  const [profile, setProfile] = useState<Profile | null>(null)
+
+  async function loadProfile() {
+    try {
+      const profile = await liff.getProfile()
+      setProfile(profile)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async function setUpLiff() {
+    try {
+      await liff.init({
+        liffId: process.env.LIFF_ID || '',
+      })
+
+      login()
+
+      loadProfile()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(
+    () => {
+      void setUpLiff()
+    },
+    []
+  )
+
   return (
     <Container>
-      <h1>TODO List</h1>
-      {error && <div>error</div>}
+      <section>
+      <h1>Profile</h1>
+        <div>{profile?.displayName}</div>
+      </section>
+      <section>
+        <h1>TODO List</h1>
+        {error && <div>error</div>}
       {loading && <div>is loading...</div>}
       <List>
         {data?.tasks.map((task: Task) => (
           <ListItem key={task.id}>{task.title}</ListItem>
         ))}
-      </List>
+        </List>
+      </section>
     </Container>
   )
 }
